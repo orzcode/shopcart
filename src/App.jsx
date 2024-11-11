845c0780828de81c21a120471ad28a31cb239a88
orzcode <71000311+orzcode@users.noreply.github.com>
2024-11-11 16:03:33 +1300

    ~

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,21 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    //#root
     <>
       <nav>
         <Link to="/">About</Link>
         <Link to="/shop">Shop</Link>
         <Link to="/cart">Cart</Link>
       </nav>
+      <header>
+        <h1>ShopCart App - I'm wrapped in a div, "header"</h1>
+      </header>
       <main>
         <Outlet /> {/* The current route content will render here */}
       </main>
+      <footer>Created by Orz <a href='https://github.com/orzcode' target='_blank'><i className='fa-brands fa-github'></i></a>
+      </footer>
     </>
   );
 }
