2d4da8d221a46080220735547ea03c609b9acc14
orzcode <71000311+orzcode@users.noreply.github.com>
2024-11-11 16:39:30 +1300

    skeleton css scaffold main page

--- src/About.jsx
+++ src/About.jsx
@@ -4,6 +4,9 @@ function About() {
 	return (
 		<div className='about'>
 		<h1 style={{color: 'red'}}>About</h1>
+		<p>This is the about page</p>
+		<p>This is the about page</p>
+
 		</div>
 	)
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Outlet, Link } from 'react-router-dom'
 
-import './App.css'
+import './App.module.css'
 
 
 function App() {
