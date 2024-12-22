0a4fd4b3087d990d6165f730721d081ec1844df7
orzcode <71000311+orzcode@users.noreply.github.com>
2024-12-22 15:09:40 +1300

    back after hiatus

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,17 @@
-import styles from './About.module.css'
+import aboutStyles from './About.module.css'
+import appStyles from './App.module.css'
+import { Outlet, Link } from 'react-router-dom'
 
 function About() {
 	return (
-		<div className='about'>
-		<h1 className={styles.fuck}>About</h1>
+		<div className={aboutStyles.aboutBox}>
+		<h1 className={aboutStyles.fuck}>The Techwood Store</h1>
 		<p>This is the about page</p>
-		<p>This is the about page</p>
+		<p>This is actually just a mockup for a react project to test some functionality.
+		The items are processed by 'fakestore API' and don't exist!</p>
+		<p>Happy 'shopping'!</p>
+
+		<Link className={appStyles.navLink} to="shop">Hit up the goods!</Link>
 
 		</div>
 	)
